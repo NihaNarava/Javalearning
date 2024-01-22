00d3cb7e2c8325aaeed7f7056d2b1c7963af2c13
Niha <kniha8240@gmail.com>
2024-01-21 21:40:48 -0500

    Add to git hub

---
+++ src/com/myunderstanding/HiWorld.java
@@ -0,0 +1,19 @@
+package com.myunderstanding;
+
+public class HiWorld {
+public static void main (String args[])
+{
+	int n=1;
+	
+	if(n==1);
+	  System.out.println(x:"Monday");
+	else if(n==2)
+		System.out.println(x:"Tuesday");
+	else if (n==3)
+		System.out.println(x:"Wednesday");
+	
+	
+}
+	
+		
+}
